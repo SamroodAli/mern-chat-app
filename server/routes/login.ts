40cbd0e9566e5075ed54aa63fa39a331d6ff02f0
samroodAli <devsamrood@gmail.com>
2022-02-04 20:01:29 +0530

    Validation: Add validation to login route to check data passed to server on login

--- server/routes/login.ts
+++ server/routes/login.ts
@@ -1,39 +1,52 @@
 import { Request, Response, Router } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
 import prisma from "../../lib/prisma";
 
 const router = Router();
-router.post("/api/users/login", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+router.post(
+  "/api/users/login",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Please enter a valid password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (user && bcrypt.compareSync(password, user.password)) {
-    const token = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-        time: Date.now(),
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
       },
-      process.env.JWT_SECRET!,
-      {
-        expiresIn: "8h",
-      }
-    );
+    });
 
-    req.session = {
-      jwt: token,
-    };
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          time: Date.now(),
+        },
+        process.env.JWT_SECRET!,
+        {
+          expiresIn: "8h",
+        }
+      );
 
-    res.status(200).json(user);
-  } else {
-    res.status(401).json({ error: "Invalid credentials" });
+      req.session = {
+        jwt: token,
+      };
+
+      res.status(200).json(user);
+    } else {
+      res.status(401).json({ error: "Invalid credentials" });
+    }
   }
-});
+);
 
 export { router as loginRouter };
