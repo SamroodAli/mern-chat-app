4cd05cc204c3152a1636b180d4e45fdcda7ec8bb
samroodAli <devsamrood@gmail.com>
2022-02-04 20:46:53 +0530

    Schema: Design database schema with through table messages with senders and recipients as users

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,21 @@
 import next, { NextApiHandler } from "next";
 import app from "./express";
 import socketServer from "./socketio";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const port: number = parseInt(process.env.PORT || "3000", 10);
-const dev: boolean = process.env.NODE_ENV !== "production";
+const port = parseInt(process.env.PORT || "3000", 10);
+const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const nextHandler: NextApiHandler = nextApp.getRequestHandler();
-import * as dotenv from "dotenv";
 
-dotenv.config();
 if (!process.env.JWT_SECRET) {
   throw new Error("JWT_SECRET must be defined");
 }
 
 nextApp.prepare().then(() => {
   const server = socketServer(app);
+
   // serve nextjs pages
   app.all("*", (req: any, res: any) => nextHandler(req, res));
 
