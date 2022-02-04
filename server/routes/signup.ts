426cb93e34830211a8c72ff8a08293655d4338ac
samroodAli <devsamrood@gmail.com>
2022-02-04 19:59:01 +0530

    Validation: Add validation to signup route to check data passed to server

--- server/routes/signup.ts
+++ server/routes/signup.ts
@@ -1,43 +1,57 @@
 import bcyrpt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { User } from "@prisma/client";
+import { body } from "express-validator";
 import prisma from "../../lib/prisma";
 import { Request, Response, Router } from "express";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = Router();
 
-router.post("/api/users/signup", async (req: Request, res: Response) => {
-  const salt = bcyrpt.genSaltSync();
-  const { username, email, password } = req.body;
+router.post(
+  "/api/users/signup",
+  [
+    body("username").isString().withMessage("Username must be present"),
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const salt = bcyrpt.genSaltSync();
+    const { username, email, password } = req.body;
 
-  let user: User;
-  try {
-    user = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password: bcyrpt.hashSync(password, salt),
+    let user: User;
+    try {
+      user = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: bcyrpt.hashSync(password, salt),
+        },
+      });
+    } catch (err) {
+      return res.status(401).json({ error: "User already exists" });
+    }
+
+    const token = jwt.sign(
+      {
+        email: user.email,
+        id: user.id,
+        time: Date.now(),
       },
-    });
-  } catch (err) {
-    return res.status(401).json({ error: "User already exists" });
+      process.env.JWT_SECRET!,
+      { expiresIn: "8h" }
+    );
+
+    req.session = {
+      jwt: token,
+    };
+
+    res.status(201).json({ data: user });
   }
-
-  const token = jwt.sign(
-    {
-      email: user.email,
-      id: user.id,
-      time: Date.now(),
-    },
-    process.env.JWT_SECRET!,
-    { expiresIn: "8h" }
-  );
-
-  req.session = {
-    jwt: token,
-  };
-
-  res.status(201).json({ data: user });
-});
+);
 
 export { router as signupRouter };
