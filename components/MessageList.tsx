6e22e71aa8fb1f23544d9ad88617a6d2ef00a60f
samroodAli <devsamrood@gmail.com>
2022-02-06 00:58:50 +0530

    Forward: Show forward button if there are messages to forward

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -5,25 +5,63 @@ const MessageList: React.FC<{ messages: Message[]; sender: User }> = ({
   messages,
   sender,
 }) => {
+  const [forwardMessages, setForwardMessages] = React.useState<String[]>([]);
+  const [forwardUsers, setForwardUsers] = React.useState<User[]>([]);
+  const [forward, setForward] = React.useState(false);
+
+  React.useEffect(() => {
+    if (forwardMessages.length) {
+      setForward(true);
+    } else {
+      setForward(false);
+    }
+  }, [forwardMessages.length]);
+
+  const handleCheck: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    if (event.target.checked) {
+      setForwardMessages((prev) => [...prev, event.target.value]);
+    } else {
+      setForwardMessages((prev) =>
+        prev.filter((message) => message !== event.target.value)
+      );
+    }
+    console.log(forwardMessages);
+  };
+
+  const handleForward: React.MouseEventHandler<HTMLButtonElement> = (e) => {};
+
   return (
-    <ul style={{ backgroundColor: "lightblue" }}>
-      {messages.map(({ id, content, senderId }) => (
-        <li
-          key={id}
-          style={{
-            color: senderId === sender?.id ? "green" : "red",
-            padding: "0.2rem",
-            margin: "0.2rem",
-            textAlign: senderId === sender?.id ? "right" : "left",
-          }}
-        >
-          <label htmlFor={id}>
-            {content}
-            <input type="checkbox" id={id} name="vehicle1" value={content} />
-          </label>
-        </li>
-      ))}
-    </ul>
+    <div>
+      {forward && (
+        <button type="button" onClick={handleForward}>
+          forward
+        </button>
+      )}
+      <ul style={{ backgroundColor: "lightblue" }}>
+        {messages.map(({ id, content, senderId }, index) => (
+          <li
+            key={id}
+            style={{
+              color: senderId === sender?.id ? "green" : "red",
+              padding: "0.2rem",
+              margin: "0.2rem",
+              textAlign: senderId === sender?.id ? "right" : "left",
+            }}
+          >
+            <label htmlFor={id}>
+              {content}
+              <input
+                type="checkbox"
+                id={id}
+                name="vehicle1"
+                value={[index.toString(), content]}
+                onChange={handleCheck}
+              />
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
